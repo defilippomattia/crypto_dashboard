0268d793a3ac8483eb268a83caf041e0a38d4913
mdefilippo <defilippomattia@gmail.com>
2021-10-09 22:52:52 +0200

    first df

--- main.py
+++ main.py
@@ -3,5 +3,25 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import requests
+import json
+from datetime import date
 
-st.title('My first app')
+st.title('Hello')
+
+ts_price_response = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=3&interval=daily')
+ts_price_dict = json.loads(ts_price_response.text)
+ts_prices = ts_price_dict["prices"]
+ts_market_caps = ts_price_dict["market_caps"]
+ts_total_volumes = ts_price_dict["total_volumes"]
+
+ts = [ts_price[0] for ts_price in ts_prices]
+ymd_date = [date.fromtimestamp(t/1000) for t in ts]
+prices = [ts_price[1] for ts_price in ts_prices]
+market_caps = [ts_market_cap[1] for ts_market_cap in ts_market_caps]
+total_volumes = [ts_total_volume[1] for ts_total_volume in ts_total_volumes]
+
+combined_data = list(map(list,zip(ts,ymd_date,prices,market_caps,total_volumes)))
+
+df = pd.DataFrame(combined_data, columns=["timestamp", "date","price","market_cap","total_volume"])
+df
